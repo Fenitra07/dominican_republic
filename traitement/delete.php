d91d4edcd35ff8f711eef3d9004ea2dcdc740b88
rasolonjatovo hery <rasolonjatovohery5@gmail.com>
2021-06-24 21:36:21 +0300

    modif delete

--- traitement/delete.php
+++ traitement/delete.php
@@ -3,10 +3,9 @@
 
 	if (!empty($_GET["id"])) {
 	 	$id = $_GET["id"]; 
-
 		$sqlDelete = "DELETE FROM demande_billet WHERE passager_id = $id;
 					  DELETE FROM profession WHERE id = $id;
-					  DELETE FROM symptome_has_passager WHERE symptome_id = $id;
+					  DELETE FROM symptome_has_passager WHERE passager_id = $id;
 					  DELETE FROM passager WHERE id = $id";
 
 		$result = $conn->query($sqlDelete);
