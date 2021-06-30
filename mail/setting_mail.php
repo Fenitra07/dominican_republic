f82fe0da0e1daefd57ae2d86f9da281f9d0fbf9b
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2021-06-30 17:03:16 +0300

    setting mail

--- mail/setting_mail.php
+++ mail/setting_mail.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
+    require_once "../PHPMailer/PHPMailer.php";
+    require_once "../PHPMailer/SMTP.php";
+    require_once "../PHPMailer/Exception.php";
+
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\SMTP;
-	require_once "PHPMailer/PHPMailer.php";
-	require_once "PHPMailer/SMTP.php";
-	require_once "PHPMailer/Exception.php";
+
 
 	$mail = new PHPMailer();
 	$mail->CharSet = PHPMailer::CHARSET_UTF8;
