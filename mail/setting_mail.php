611285d1fa9a1529b1fb1ba38b1196e4882bd112
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2021-07-08 15:44:25 +0300

    swift mailer

--- mail/setting_mail.php
+++ mail/setting_mail.php
@@ -9,13 +9,13 @@
 
 	$mail = new PHPMailer(true);
 	$mail->CharSet = PHPMailer::CHARSET_UTF8;
-	$mail->SMTPDebug = SMTP::DEBUG_CONNECTION;
+	$mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;
     //Server settings
     $mail->isSMTP();
     $mail->Host = "mail.go-dominican-republic.com";
-    $mail->SMTPAuth = false;
+//    $mail->SMTPAuth = true;
     $mail->Username = "no-reply@go-dominican-republic.com";
     $mail->Password = 'A741852*/';
     $mail->Port = 465;
-//    $mail->SMTPSecure = "ssl";
+    $mail->SMTPSecure = "ssl";
  ?>
