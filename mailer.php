33cdccaaa08490969d3adde3294ece79adccf13f
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2021-07-08 15:47:08 +0300

    execption mailer

--- mailer.php
+++ mailer.php
@@ -2,20 +2,25 @@
 
 require_once './vendor/autoload.php';
 // Create the Transport
-$transport = (new Swift_SmtpTransport('mail.go-dominican-republic.com', 2079))
+try {
+
+    $transport = (new Swift_SmtpTransport('mail.go-dominican-republic.com', 2079))
 //    ->setUsername('no-reply@go-dominican-republic.com')
 //    ->setPassword('A741852*/')
-;
+    ;
 
 // Create the Mailer using your created Transport
-$mailer = new Swift_Mailer($transport);
+    $mailer = new Swift_Mailer($transport);
 
 // Create a message
-$message = (new Swift_Message('Wonderful Subject'))
-    ->setFrom(['no-reply@go-dominican-republic.com' => 'John Doe'])
-    ->setTo(['harinjatovo.fetra@gmail.com', 'tounafyoung@gmail.com' => 'Fetra'])
-    ->setBody('Here is the message itself')
-;
+    $message = (new Swift_Message('Wonderful Subject'))
+        ->setFrom(['no-reply@go-dominican-republic.com' => 'John Doe'])
+        ->setTo(['harinjatovo.fetra@gmail.com', 'tounafyoung@gmail.com' => 'Fetra'])
+        ->setBody('Here is the message itself')
+    ;
 
 // Send the message
-$result = $mailer->send($message);
+    $result = $mailer->send($message);
+} catch (Exception $exception) {
+    var_dump($exception);
+}
